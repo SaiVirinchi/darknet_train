ddc906489bc4a20dcc768160e94422b95be0257b
SaiVirinchi <52707426+SaiVirinchi@users.noreply.github.com>
2019-07-14 12:35:22 +0530

    Update train_maker.py

--- train_maker.py
+++ train_maker.py
@@ -1,25 +1,20 @@
 import os
-
-path='[enter path of your directory where the images are stored]'
-
-while True:
-    image= input()
-    imgList=os.listdir(image)
-
-    print(imgList)
-
-    textFile=open('trainy.txt','a')
-
-
-    for img in imgList:
-        file = os.path.splitext(img)[1]
-        if file == '.JPG':
-            imgPath=path+image+'/'+ img +'\n'
-            textFile.write(imgPath)
-            print('success')
-        elif file == '.jpg':
-            imgPath=path+image+'/'+ img +'\n'
-            textFile.write(imgPath)
-            print('success')
-        else:
-            print(file)
+path=os.path.dirname(os.path.abspath(__file__))
+#path='[enter path of your directory where the images are stored]'
+print('Your Image folder name
+image= input()
+imgList=os.listdir(image)
+print(imgList)
+textFile=open('trainy.txt','a')
+for img in imgList:
+    file = os.path.splitext(img)[1]
+    if file == '.JPG':
+        imgPath=path+image+'/'+ img +'\n'
+        textFile.write(imgPath)
+        print('success')
+    elif file == '.jpg':
+        imgPath=path+image+'/'+ img +'\n'
+        textFile.write(imgPath)
+        print('success')
+    else:
+        print(file)
